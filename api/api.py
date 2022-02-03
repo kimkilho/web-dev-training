4bfb6c87c80e8bc1daee82f6f49b92252f06e251
Kyle Kim <Kyle.Kim@cognex.com>
2022-02-03 19:58:35 +0900

    Add returning an image file in the API

--- api/api.py
+++ api/api.py
@@ -1,15 +1,42 @@
+import os
+import os.path as osp
+import io, base64
 from typing import Optional
+import random
+random.seed(2022)
 
+import numpy as np
+from PIL import Image
 from fastapi import FastAPI
+from starlette.responses import StreamingResponse
 
+
+img_dir = osp.join(os.getcwd(), '..', 'images')
 app = FastAPI()
 
 
-@app.get('/')
+def encode_img(img):
+    """img: PIL.Image"""
+    img_bytes = io.BytesIO()
+    img.save(img_bytes, format='PNG')
+    img_encoded = base64.encodebytes(img_bytes.getvalue()).decode('ascii')
+
+    return img_encoded
+
+
+@app.get('/api/')
 def read_root():
-    return {'Hello': 'World'}
+    filenames = os.listdir(img_dir)
+    # random.shuffle(filenames)
+
+    return filenames
 
 
-@app.get('/items/{item_id}')
-def read_item(item_id: int, q: Optional[str] = None):
-    return {'item_id': item_id, 'q': q}
+@app.get('/api/{filename}')
+def read_file(filename: str):
+    file_path = osp.join(img_dir, filename)
+    img = Image.open(file_path).convert('RGBA')
+    img_encoded = encode_img(img)
+
+    return StreamingResponse(img_encoded)
+
