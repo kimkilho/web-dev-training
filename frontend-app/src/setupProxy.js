98fff83da591ff5087c083faeab72cfdc911eab9
kilho <catharsistk@gmail.com>
2022-02-04 10:42:11 +0900

    Replace 'proxy' key-value with setupProxy script

---
+++ frontend-app/src/setupProxy.js
@@ -0,0 +1,13 @@
+// Reference: https://stackoverflow.com/questions/70374005/invalid-options-object-dev-server-has-been-initialized-using-an-options-object/70413065#70413065
+
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = function(app) {
+  app.use(
+    '/',
+    createProxyMiddleware({
+      target: 'http://localhost:8000',
+      changeOrigin: true,
+    })
+  )
+}
