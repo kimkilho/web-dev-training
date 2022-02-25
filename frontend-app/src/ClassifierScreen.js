dec5af9e0cc4cc9ddf628e51f44aa694ad1fadc5
kilho <catharsistk@gmail.com>
2022-02-25 14:14:27 +0900

    Add image file selection and rendering in ClassifierScreen; Add css for ClassifierScreen

--- frontend-app/src/ClassifierScreen.js
+++ frontend-app/src/ClassifierScreen.js
@@ -1,8 +1,52 @@
+import { useState, useRef } from 'react';
+import './ClassifierScreen.css';
+
 function ClassifierScreen() {
+  const [imageURL, setImageURL] = useState(null);
+  const [result, setResult] = useState('');
+  const imageRef = useRef();
+
+  const uploadImage = async (e) => {
+    // console.log('uploadImage() called');
+    const { files } = e.target;
+    if (files.length > 0) {
+      const file = files[0];
+      imageRef.current = file;
+      const url = URL.createObjectURL(file);
+      setImageURL(url);
+    }
+  }
+
+  const predict = async () => {
+    console.log('predict() called');
+  }
+
   return (
     <>
-      <h1>Image Classifier Tool</h1>
-      <p>(To be implemented)</p>
+      <h1 className="header">Classifier Tool</h1>
+      <div className="input-holder">
+        <input
+          type="file"
+          accept="image/*"
+          onChange={uploadImage}
+        />
+      </div>
+      <div className="image-wrapper">
+        <div className="image-holder">
+          {imageURL &&
+            <img
+              src={imageURL}
+              alt="Image"
+              crossOrigin="anonymous"
+            />
+          }
+        </div>
+      </div>
+      <div className="button-holder">
+        {imageURL && <button className="button" onClick={predict}>
+          Predict</button>}
+        <p>Prediction result: <b>{result}</b></p>
+      </div>
     </>
   );
 }
